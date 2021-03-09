bb588fb43ee3a26a05b1e7e3206cc320f08860fd
glnnrhodes <glnnrhodes@gmail.com>
2021-03-08 22:12:19 -0700

    Better parallel

--- Code/main.c
+++ Code/main.c
@@ -23,6 +23,7 @@
 int thread_count;
 int serial(double** Au, int size);
 int parallel(double** Au, int size);
+int elimination(int num_count);
 
 /*--------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
@@ -41,17 +42,18 @@ int main(int argc, char* argv[]) {
 
     //TO DO Parallel implementation using OpenMP
     parallel(Au, size);
+    //elimination(thread_count);
     //serial(Au, size);
     return 0;
 }  /* main */
 
 int parallel(double** Au, int size){
-/*Calculate the solution by serial code*/
+/*Calculate the solution by parallel code*/
     double start, finish;
     GET_TIME(start);
-    int i, j, k;
+    int i, j, k, ind, l_ind;
 	double* X;
-	double temp;
+	double temp, max, l_max;
 	int* index;
 	X = CreateVec(size);
     index = malloc(size * sizeof(int));
@@ -63,50 +65,72 @@ int parallel(double** Au, int size){
         X[0] = Au[0][1] / Au[0][0];
     else{
         /*Gaussian elimination*/
-        for (k = 0; k < size - 1; ++k){
-            /*Pivoting*/
-            temp = 0;
-            for (i = k, j = 0; i < size; ++i){
-                if (temp < Au[index[i]][k] * Au[index[i]][k]){
-                    temp = Au[index[i]][k] * Au[index[i]][k];
-                    j = i;
-                } 
+        # pragma omp parallel num_threads(thread_count) shared(Au, size, X, index, max, ind) private(j, i, temp, k, l_max, l_ind)
+        {
+            for (k = 0; k < size - 1; ++k){
+                /*Pivoting*/
+                max = 0;
+                ind = 0;
+                # pragma omp for
+                for (i = k; i < size; i++){ //Find local max and index of max for each thread
+                    if (max < Au[index[i]][k] * Au[index[i]][k]){
+                        l_max = Au[index[i]][k] * Au[index[i]][k]; 
+                        l_ind = i;
+                    } 
+                }
+
+                if (l_max > max){
+                    #pragma omp critical
+                    {
+                        if (l_max > max){ //Double check still true once in critical
+                            max = l_max;
+                            ind = l_ind;
+                        }
+                    }
+                }
+                # pragma omp barrier
+                # pragma omp single
+                {
+                    if (ind != k)/*swap*/{
+                        i = index[ind];
+                        index[ind] = index[k];
+                        index[k] = i;
+                    }
+                }
+            
+                /*Elimination*/
+                # pragma omp for
+                for (i = k + 1; i < size; ++i){
+                    temp = Au[index[i]][k] / Au[index[k]][k];
+                    for (j = k; j < size + 1; ++j)
+                        Au[index[i]][j] -= Au[index[k]][j] * temp;
+                }
             }
-            if (j != k)/*swap*/{
-                i = index[j];
-                index[j] = index[k];
-                index[k] = i;
+            // #pragma omp single
+            // {
+            //     PrintMat(Au, size, size);
+            // }
+
+            /*Jordan elimination*/
+            for (k = size - 1; k > 0; --k){
+            
+                # pragma omp for //Need fixed k for paralle elimination
+                for (i = k - 1; i >= 0; --i ){
+                    temp = Au[index[i]][k] / Au[index[k]][k];
+                    Au[index[i]][k] -= temp * Au[index[k]][k];
+                    Au[index[i]][size] -= temp * Au[index[k]][size];
+                }
             }
 
-            /*Elimination*/
-            for (i = k + 1; i < size; ++i){
-                temp = Au[index[i]][k] / Au[index[k]][k];
-                
-                # pragma omp parallel for num_threads(thread_count)
-                for (j = k; j < size + 1; ++j)
-                    Au[index[i]][j] -= Au[index[k]][j] * temp;
-            }     
-        }
-        /*Jordan elimination*/
-        for (k = size - 1; k > 0; --k){
-        
-            # pragma omp parallel for num_threads(thread_count)
-            for (i = k - 1; i >= 0; --i ){
-                temp = Au[index[i]][k] / Au[index[k]][k];
-                Au[index[i]][k] -= temp * Au[index[k]][k];
-                Au[index[i]][size] -= temp * Au[index[k]][size];
-            }
-        }
-        /*solution*/
-        # pragma omp parallel for num_threads(thread_count)
-        for (k=0; k< size; ++k)
-            X[k] = Au[index[k]][size] / Au[index[k]][k];
+            /*solution*/
+            # pragma omp for
+            for (k=0; k< size; ++k)
+                X[k] = Au[index[k]][size] / Au[index[k]][k];
 
+        }   
     }
-
     GET_TIME(finish);
     Lab3SaveOutput(X, size, finish-start);
-
 	
     DestroyVec(X);
     DestroyMat(Au, size);
